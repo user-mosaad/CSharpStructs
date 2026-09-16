1e6de6b2d170db4cfd3885aa1e5f68787b5e75bc
user-mosaad <msaadshahid999@gmail.com>
2026-09-16 21:39:36 +0600

    Add project files.

---
+++ CSharpStructs/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharpStructs;
+
+class Program
+{
+    static void Main()
+    {
+        User user1 = new("John Doe", "john", AccountType.Cashier);
+        Usery usery1 = new Usery("Michael", "michael", AccountType.Manager);
+        Console.WriteLine(user1);
+        Console.WriteLine(usery1);
+    }
+}
---
+++ CSharpStructs/User.cs
@@ -0,0 +1,56 @@
+﻿namespace CSharpStructs;
+
+enum AccountType
+{
+    Manager,
+    Cashier
+}
+
+struct User
+{
+    // Fields and Properties
+    public string Name { get; set; }
+    public string Username { get; set; }
+    public AccountType UserAccountType { get; set; }
+
+    // Constructor
+    public User(string name, string username, AccountType userAccountType)
+    {
+        Name = name;
+        Username = username;
+        UserAccountType = userAccountType;
+    }
+
+    // Methods
+    public void ChangeAccountType(AccountType newAccountType)
+    {
+        UserAccountType = newAccountType;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}, Username: {Username}, Account Type: {UserAccountType}";
+    }
+}
+
+readonly struct Usery
+{
+    // Fields and Properties
+    public string Name { get; }
+    public string Username { get; }
+    public AccountType UserAccountType { get; }
+
+    // Constructor
+    public Usery(string name, string username, AccountType userAccountType)
+    {
+        Name = name;
+        Username = username;
+        UserAccountType = userAccountType;
+    }
+
+    // Methods
+    public override string ToString()
+    {
+        return $"Name: {Name}, Username: {Username}, Account Type: {UserAccountType}";
+    }
+}
